5050339dce7d92d5632eacbd899e298d80e4dbe2
wjhong1234 <wjhong@shaw.ca>
2017-02-21 22:39:55 -0700

    Added Starting and Final blocks
    I'm thinking we can use final block to check for victory conditions but iunno shit.

---
+++ GamePieces/FinalBlock.java
@@ -0,0 +1,11 @@
+/* This is a class representing the Final block of the puzzle. 
+ * We can check for victory conditions here(?). */
+
+public class FinalBlock {
+	
+	//Constructor takes in a coordinate pair and passes to parent.
+	public FinalBlock(int x, int y) {
+		super(x, y);
+	}
+}
+
---
+++ GamePieces/StartingBlock.java
@@ -0,0 +1,10 @@
+/* This is a class representing the starting block in the puzzle. */
+
+public class StartingBlock extends Block {
+	
+	//Constructor takes in a coordinate pair and passes to parent.
+	public StartingBlock(int x, int y) {
+		super(x, y);
+	}
+}
+
