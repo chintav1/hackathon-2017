6dd0726943001a1ec10ecce872603cbae0f81070
eayuban <edraelan.ayuban@ucalgary.ca>
2017-02-21 22:56:03 -0700

    added 'StartingBlock' to 'GameLogic' package

--- GamePieces/StartingBlock.java
+++ GamePieces/StartingBlock.java
@@ -1,5 +1,5 @@
 /* This is a class representing the starting block in the puzzle. */
-
+package GameLogic;
 public class StartingBlock extends Block {
 	
 	//Constructor takes in a coordinate pair and passes to parent.
