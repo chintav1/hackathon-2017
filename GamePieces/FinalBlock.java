29e7ad1a0b3abe2dfee60e720d2cc6d18cf89812
eayuban <edraelan.ayuban@ucalgary.ca>
2017-02-21 23:25:10 -0700

    Added extension to 'Block' class

--- GamePieces/FinalBlock.java
+++ GamePieces/FinalBlock.java
@@ -1,7 +1,7 @@
 /* This is a class representing the Final block of the puzzle. 
  * We can check for victory conditions here(?). */
 package GameLogic;
-public class FinalBlock {
+public class FinalBlock extends Block{
 	
 	//Constructor takes in a coordinate pair and passes to parent.
 	public FinalBlock(int x, int y) {
